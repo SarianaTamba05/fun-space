6cbd048f78862a0f4ae25e3c53f8b35e71a23d11
Sariana Parnauli Tamba <parnauli05@gmail.com>
2024-03-04 20:59:57 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,7 @@
+#menghitung luas segitiga sederhana
+def luas_segitiga():
+    alas= int(input("Masukkan alas segitiga: "))
+    tinggi= int(input("Masukkan tinggi segitiga: "))
+    luas = alas * tinggi / 2
+    print("Luas segitiga adalah: ", luas)
+luas_segitiga()
